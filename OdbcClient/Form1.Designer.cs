d4eb8809599bf83961cc601dee0fc0e846180c53
Tetens <clte@sdc.dk>
2017-12-21 12:47:40 +0100

    Yet another RDB-viewer

---
+++ OdbcClient/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace OdbcClient {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.userIdTextBox = new System.Windows.Forms.TextBox();
+            this.odbcComboBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1270, 653);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1262, 627);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Home";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteCustomSource.AddRange(new string[] {
+            "Select",
+            "From",
+            "Into",
+            "Delete",
+            "Insert",
+            "Count",
+            "Max",
+            "Sum"});
+            this.textBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(3, 146);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(630, 478);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.textBox1.Location = new System.Drawing.Point(633, 146);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(626, 478);
+            this.textBox1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.passwordTextBox);
+            this.panel1.Controls.Add(this.userIdTextBox);
+            this.panel1.Controls.Add(this.odbcComboBox);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1256, 143);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(663, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(676, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "UserID";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(722, 56);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(132, 20);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // userIdTextBox
+            // 
+            this.userIdTextBox.Location = new System.Drawing.Point(722, 30);
+            this.userIdTextBox.Name = "userIdTextBox";
+            this.userIdTextBox.Size = new System.Drawing.Size(132, 20);
+            this.userIdTextBox.TabIndex = 2;
+            // 
+            // odbcComboBox
+            // 
+            this.odbcComboBox.FormattingEnabled = true;
+            this.odbcComboBox.Location = new System.Drawing.Point(1090, 27);
+            this.odbcComboBox.Name = "odbcComboBox";
+            this.odbcComboBox.Size = new System.Drawing.Size(121, 21);
+            this.odbcComboBox.TabIndex = 1;
+            this.odbcComboBox.SelectedIndexChanged += new System.EventHandler(this.odbcComboBox_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(500, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go do";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1262, 627);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Result";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1256, 621);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1262, 627);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1270, 653);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox odbcComboBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox userIdTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
---
+++ OdbcClient/Form1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OdbcClient {
+    public partial class Form1 : Form {
+        string odbcConnectionName = "DB2A";
+        public Form1() {
+            InitializeComponent();
+            odbcDataSources();
+            setTextBoxValues();
+        }
+
+        private void odbcDataSources() {
+            Microsoft.Win32.RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("Software\\ODBC\\ODBC.INI\\ODBC Data Sources");
+
+            List<string> odbcNameList = new List<string>();
+            odbcNameList.AddRange(registryKey.GetValueNames());
+            odbcNameList.Sort();
+            odbcComboBox.Items.AddRange(odbcNameList.ToArray());
+            odbcComboBox.SelectedItem = odbcConnectionName;
+        }
+        private void button1_Click(object sender, EventArgs e) {
+            //dataGridView1.Columns.Add("ColumnName1","ColumnHeader1");
+            //dataGridView1.Columns.Add("ColumnName2", "ColumnHeader2");
+            //dataGridView1.Columns.Add("ColumnName3", "ColumnHeader3");
+            execSql();
+            tabControl1.SelectedTab=tabPage2;
+        }
+
+        private void setTextBoxValues() {
+            userIdTextBox.Text = "U3249";
+            passwordTextBox.Text = "";
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e) {
+
+        }
+
+        private void odbcComboBox_SelectedIndexChanged(object sender, EventArgs e) {
+            odbcConnectionName = (string)odbcComboBox.SelectedItem;
+        }
+
+        private void execSql() {
+            //h ttp://stackoverflow.com/questions/13257401/c-sharp-prepared-statement-and-multiple-queries
+            string myConnectionString = "DSN=" + odbcConnectionName + ";UID=" + userIdTextBox.Text + ";PWD=" + passwordTextBox.Text + ";";
+            OdbcConnection myConnection = new OdbcConnection();
+            myConnection.ConnectionString = myConnectionString;
+            myConnection.Open();
+            OdbcCommand odbcCommand = myConnection.CreateCommand();
+
+            odbcCommand.CommandText = "select count(*) from testc.vpfact_fad";
+            object obj = odbcCommand.ExecuteScalar();
+
+
+            odbcCommand.Parameters.Clear();
+
+            //odbcCommand.Parameters.Add("?ID7", OdbcType.VarChar).Value = parameter7;
+            //queryStatement1() returns the query string insert into TABLE (field1, field2...) values(?,?,...)
+
+            odbcCommand.CommandText = "select * from testc.vpfact_fad fetch first 3 rows only";
+            OdbcDataReader odbcDataReader = odbcCommand.ExecuteReader();
+
+            textBox1.AppendText("RowCount a: " + dataGridView1.RowCount + "\n");
+
+            //dataGridView1.ColumnCount = odbcDataReader.FieldCount;
+            for(int i = 0; i < odbcDataReader.FieldCount; ++i)
+                dataGridView1.Columns.Add(odbcDataReader.GetName(i), odbcDataReader.GetName(i));
+            textBox1.AppendText("RowCount b: " + dataGridView1.RowCount + "\n");
+
+            for (int i = 0; i < odbcDataReader.FieldCount; ++i) textBox1.AppendText("[" + i + "] " + odbcDataReader.GetName(i) + "   ");
+
+
+            //h ttps://msdn.microsoft.com/en-us/library/system.windows.forms.datagridview.columns%28v=vs.110%29.aspx
+
+            while(odbcDataReader.Read()) {
+                int row = dataGridView1.Rows.Add();
+
+                for(int i = 0; i < odbcDataReader.FieldCount; ++i) {
+                    dataGridView1.Rows[row].Cells[i].Value = odbcDataReader.GetString(i);
+
+                }
+            }
+            textBox1.AppendText("RowCount c: " + dataGridView1.RowCount + "\n");
+
+            myConnection.Close();
+        }
+    }
+}
